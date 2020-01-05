d146c64dd2100b3f015820a8ae6ac61e8626297c
Simran maken <abc@xyz.com>
2020-01-05 21:12:38 +0530

    Additon of 2 and 3 numbers

---
+++ Add.py
@@ -0,0 +1,10 @@
+#Addition
+
+def add2(num1, num2):
+	return num1 + num2
+
+def add3(num1, num2, num3):
+	return num1 + num2 + num3
+
+print("Addition of two numbers: ", add2(10, 20))
+print("Addition of three numbers: ", add3(10, 20, 30))
