d4dde18a2d20c65f8367ceeff7ad92057689571e
one <one@one.com>
2020-01-05 22:31:02 +0530

    Adds sub3 in Sub.py

--- Sub.py
+++ Sub.py
@@ -3,4 +3,8 @@
 def sub2(num1, num2):
 	return num1 - num2
 
+def sub3(num1, num2, num3):
+	return num1 - num2 - num3
+
 print("Substraction of two numbers: ", sub2(20, 10))
+print("Substraction of three numbers: ", sub3(10, 20, 30))
