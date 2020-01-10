c8358c285cfa6ee19e23d230fc9f9e28d46fb57d
one <one@one.com>
2020-01-10 17:57:09 +0530

    Removes add3 from Add.txt file

--- Add.py
+++ Add.py
@@ -3,8 +3,4 @@
 def add2(num1, num2):
 	return num1 + num2
 
-def add3(num1, num2, num3):
-	return num1 + num2 + num3
-
 print("Addition of two numbers: ", add2(10, 20))
-print("Addition of three numbers: ", add3(10, 20, 30))
