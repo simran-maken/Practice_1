bda5cf6a86a8594357ccb3044bb571cfbb0a49ff
one <one@one.com>
2020-01-10 17:42:34 +0530

    Revert "Adds sub3 in Sub.py"
    This reverts commit d4dde18a2d20c65f8367ceeff7ad92057689571e.

--- Sub.py
+++ Sub.py
@@ -3,8 +3,4 @@
 def sub2(num1, num2):
 	return num1 - num2
 
-def sub3(num1, num2, num3):
-	return num1 - num2 - num3
-
 print("Substraction of two numbers: ", sub2(20, 10))
-print("Substraction of three numbers: ", sub3(10, 20, 30))
